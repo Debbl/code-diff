48ac2d29b1b4bd88b323c4c95e41d747e78ba9d9
Debbl <me@aiwan.run>
2023-03-26 16:29:59 +0800

    feat: layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { DiffEditor } from "@monaco-editor/react";
+import Head from "next/head";
+import Footer from "~/components/Footer";
 import type { Theme } from "~/hooks/useDiffEditor";
 import useDiffEditor from "~/hooks/useDiffEditor";
 
@@ -9,64 +11,72 @@ export default function Index() {
   ] = useDiffEditor();
 
   return (
-    <div className="h-screen">
-      <h1 className="text-center my-2 text-xl font-medium">Code Diff</h1>
+    <>
+      <Head>
+        <title>code-diff</title>
+        <link rel="shortcut icon" href="/logo.svg" type="image/x-icon" />
+      </Head>
+      <div className="h-screen flex flex-col">
+        <h1 className="text-center my-2 text-xl font-medium">Code Diff</h1>
 
-      <div className="p-3 flex gap-x-3">
-        <div>
-          <label>
-            选择语言：
-            <select
-              value={language}
-              onChange={handleChange}
-              className="border ml-3 w-60"
-            >
-              {languages.map((lang) => (
-                <option value={lang.id} key={lang.id}>
-                  {lang.id}
-                </option>
-              ))}
-            </select>
-          </label>
+        <div className="p-3 flex gap-x-3">
+          <div>
+            <label>
+              选择语言：
+              <select
+                value={language}
+                onChange={handleChange}
+                className="border ml-3 w-60"
+              >
+                {languages.map((lang) => (
+                  <option value={lang.id} key={lang.id}>
+                    {lang.id}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </div>
+          <div>
+            <label>
+              选择主题：
+              <select
+                value={theme}
+                onChange={(e) => setTheme(e.target.value as Theme)}
+                className="border ml-3 w-60"
+              >
+                <option value="light">light</option>
+                <option value="vs-dark">vs-dark</option>
+              </select>
+            </label>
+          </div>
+          <div>
+            <label>
+              并排显示：
+              <input
+                className="ml-3"
+                type="checkbox"
+                checked={renderSideBySide}
+                onChange={(e) => setRenderSideBySide(e.target.checked)}
+              />
+            </label>
+          </div>
         </div>
-        <div>
-          <label>
-            选择主题：
-            <select
-              value={theme}
-              onChange={(e) => setTheme(e.target.value as Theme)}
-              className="border ml-3 w-60"
-            >
-              <option value="light">light</option>
-              <option value="vs-dark">vs-dark</option>
-            </select>
-          </label>
-        </div>
-        <div>
-          <label>
-            并排显示：
-            <input
-              className="ml-3"
-              type="checkbox"
-              checked={renderSideBySide}
-              onChange={(e) => setRenderSideBySide(e.target.checked)}
-            />
-          </label>
-        </div>
-      </div>
 
-      <div className="h-full">
-        <DiffEditor
-          options={{
-            originalEditable: true,
-            renderSideBySide,
-          }}
-          onMount={handleOnMount}
-          height="100%"
-          theme={theme}
-          language={language}
-        />
+        <div className="h-full">
+          <DiffEditor
+            options={{
+              originalEditable: true,
+              renderSideBySide,
+            }}
+            onMount={handleOnMount}
+            height="100%"
+            theme={theme}
+            language={language}
+          />
+        </div>
+
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
